480a1d5f4355df9d12fe1b5f31b92972e9602227
Al Biheiri <abiheiri@apple.com>
2020-06-27 00:50:52 -0700

    WIP: getToken

--- inv.go
+++ inv.go
@@ -3,16 +3,45 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"strings"
 )
 
 func main() {
-    b, err := ioutil.ReadFile("auth.conf") // just pass the file name
-    if err != nil {
-        fmt.Print(err)
-    }
+	b, err := ioutil.ReadFile("auth.conf") // just pass the file name
+	if err != nil {
+		fmt.Print(err)
+	}
 
-    s := strings.Split(string(b),",")
-    username, key := s[0], s[1]
-    fmt.Println(username, key)
+	s := strings.Split(string(b), ",")
+
+	username, apikey := s[0], s[1]
+	// fmt.Println(username, apikey)
+
+	getToken(username, apikey)
+}
+
+func getToken(username, apikey string) {
+	url := "https://accounts.zoho.com/apiauthtoken/nb/create?SCOPE=ZohoInventory/inventoryapi&EMAIL_ID=" + username + "&PASSWORD=" + apikey
+	method := "GET"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	res, err := client.Do(req)
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+
+	// s := strings.Split(string(body),"=")
+	fmt.Println(string(body))
+	// fmt.Println(s[1])
+
+	//r := regexp.MustCompile(`(?P<first>.*)`)
+	// fmt.Printf("%#v\n", r.FindStringSubmatch(`2015-05-27`))
+	//fmt.Printf(string(body), r.SubexpNames())
+
 }
