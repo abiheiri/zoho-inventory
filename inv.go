7b447569b76dbbe0353dd9afabdbe0e35c7d028e
Al Biheiri <abiheiri@apple.com>
2020-06-27 23:43:40 -0700

    setting up flags

--- inv.go
+++ inv.go
@@ -4,22 +4,56 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"regexp"
-	"strings"
+    "strings"
+    "flag"
 )
 
 func main() {
-	b, err := ioutil.ReadFile("auth.conf") // just pass the file name
+
+	f, err := ioutil.ReadFile("auth.conf")
 	if err != nil {
 		fmt.Print(err)
 	}
 
-	s := strings.Split(string(b), ",")
+	s := strings.Split(string(f), ",")
 
 	username, apikey := s[0], s[1]
 	// fmt.Println(username, apikey)
 
-	getToken(username, apikey)
+    // Is there an existing token?
+	t, err := ioutil.ReadFile("token.txt")
+	if err != nil {
+		getToken(username, apikey)
+	}
+
+	token := (string(t))
+	fmt.Println(token)
+
+    // Create flags
+    // inv -g -i=all
+    getPtr := flag.String("g", "get", "a string")
+    // numbPtr := flag.Int("numb", 42, "an int")
+    // boolPtr := flag.Bool("fork", false, "a bool")
+
+    // var svar string
+    // flag.StringVar(&svar, "svar", "bar", "a string var")
+    flag.Parse()
+
+    fmt.Println("word:", *getPtr)
+    // fmt.Println("numb:", *numbPtr)
+    // fmt.Println("fork:", *boolPtr)
+    // fmt.Println("svar:", svar)
+    fmt.Println("tail:", flag.Args())
+
+
+    if getPtr != nil {
+        fmt.Println("word:", *getPtr)
+    }
+
+    
+
 }
 
 func getToken(username, apikey string) {
@@ -37,20 +71,27 @@ func getToken(username, apikey string) {
 	defer res.Body.Close()
 	body, err := ioutil.ReadAll(res.Body)
 
-	// s := strings.Split(string(body),"=")
-	// fmt.Println(string(body))
-	// fmt.Println(s[1])
 
 	var re = regexp.MustCompile(`AUTHTOKEN=(?P<token>\S+)`)
 	var str = string(body)
 	match := re.FindStringSubmatch(str)
 	fmt.Printf("TOKEN: %s", match[1])
 
-	// r, err := regexp.MatchString(`AUTHTOKEN=(.*)`, string(body))
-
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-
-	// fmt.Println(string(r))
+	f, err := os.Create("token.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	l, err := f.WriteString(match[1])
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return
+	}
+	fmt.Println(l, "saved a new token successfully")
+	err = f.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
